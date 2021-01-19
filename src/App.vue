fd99fa1a73da4beb7950f380851f80a18de7d6d4
Pratik Makvana <pratik.makvana@emperorbrains.com>
2021-01-19 10:42:52 +0530

    [Added] Initial Load Image

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <LoadImage />
+  </div>
+</template>
+
+<script>
+import LoadImage from './components/LoaderImage.vue'
+export default {
+  name: 'App',
+  components: {
+    LoadImage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/LoaderImage.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <h2>Image Loader Spinner</h2>
+        <button @click="toggleimg">{{ btntext }}</button>
+        <div v-if="isLoading" class="loading"></div>
+        <img v-show="isLoaded" :src="imgsrc" width="400" @load="loaded">
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            imgsrc: "https://placeimg.com/500/300/nature",
+            btntext: "Show Image",
+            isLoaded: false,
+            isLoading: false
+        }
+    },
+    methods: {
+        loaded() {
+            this.isLoaded = true;
+            this.isLoading = false;
+        },
+        toggleimg: function(){
+            if (this.imgsrc == "") {
+                this.isLoaded =  false;
+                this.isLoading = true;
+                this.imgsrc = "https://placeimg.com/500/300/nature?t=" + Math.random()
+                // this.imgsrc = "../assets/logo.png"
+                this.btntext = "Hide Image"
+            }else{
+                this.imgsrc = ""
+                this.btntext = "Show Image"
+            }
+        }
+    }
+}
+</script>
+
+<style>
+    .loading {
+        background: transparent url('https://miro.medium.com/max/882/1*9EBHIOzhE1XfMYoKz1JcsQ.gif') center no-repeat;
+        height: 200px;
+        width: 200px;
+    }
+</style>
